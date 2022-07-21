7631259edba19440bbcc1dba61b9ef51e5da829e
mhdkurdish <49838432+mhdkurdish@users.noreply.github.com>
2022-07-22 02:26:12 +0300

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -53,8 +53,8 @@ async def _start(client, message):
         reply_to_message_id=message.message_id
         )
             return
-    await client.send_message(message.chat.id,
-	reply_photo="https://telegra.ph/file/c934a8b82dc2963b6de45.jpg",		      
+    await client.send_photo(message.chat.id,
+	"https://telegra.ph/file/c934a8b82dc2963b6de45.jpg",		      
         text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
 	reply_markup=InlineKeyboardMarkup(
             [
